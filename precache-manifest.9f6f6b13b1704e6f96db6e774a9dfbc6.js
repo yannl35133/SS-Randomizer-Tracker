7928dcb5e6b7280ceb892212bf49a6719b835589
Floha <floha2011@hotmail.de>
2020-10-22 04:21:45 +0200

    Updates

---
+++ precache-manifest.9f6f6b13b1704e6f96db6e774a9dfbc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da39c8825c39c883e8dae1dff9ae690f",
+    "url": "/SS-Randomizer-Tracker/index.html"
+  },
+  {
+    "revision": "11ff5e93deea7baef253",
+    "url": "/SS-Randomizer-Tracker/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "4e2c422cfa22ff46828e",
+    "url": "/SS-Randomizer-Tracker/static/js/2.a33a10e4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/SS-Randomizer-Tracker/static/js/2.a33a10e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11ff5e93deea7baef253",
+    "url": "/SS-Randomizer-Tracker/static/js/main.5d510fee.chunk.js"
+  },
+  {
+    "revision": "79d08acb5e0b22c67d07",
+    "url": "/SS-Randomizer-Tracker/static/js/runtime-main.ec47c787.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/SS-Randomizer-Tracker/static/media/logo.ee7cd8ed.svg"
+  }
+]);
